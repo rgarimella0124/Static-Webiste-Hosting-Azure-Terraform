679fe28d2099f1cd4ad63bc9ccac1f10cc350c8b
Ram Simran Garimella (Tata Consultancy Services Limi) <v-garimellar@microsoft.com>
2021-11-26 12:33:09 +0530

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_client_config" "current" {}
+
+#Create Resource Group
+resource "azurerm_resource_group" "resource_group" {
+  name     = var.resource_group
+  location = var.location
+}
+
+#Create Storage account
+resource "azurerm_storage_account" "storage_account" {
+  name                = var.storage_account
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  static_website {
+    index_document = "index.html"
+  }
+}
+
+#Add index.html to blob storage
+resource "azurerm_storage_blob" "example" {
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.storage_account.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  content_type           = "text/html"
+  source                 = "index.html"
+}
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+location        = "eastus2"
+resource_group  = "workload-rg"
+storage_account = "simranstgaccount"
