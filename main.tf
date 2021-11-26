d8d71ebdc44607f9656932007fcbddf11dd53097
Ram Simran Garimella (Tata Consultancy Services Limi) <v-garimellar@microsoft.com>
2021-11-26 13:40:01 +0530

    Added Error File

--- main.tf
+++ main.tf
@@ -12,23 +12,33 @@ resource "azurerm_resource_group" "resource_group" {
 
 #Create Storage account
 resource "azurerm_storage_account" "storage_account" {
-  name                = var.storage_account
-  resource_group_name = azurerm_resource_group.resource_group.name
+  name                     = var.storage_account
+  resource_group_name      = azurerm_resource_group.resource_group.name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   account_kind             = "StorageV2"
   static_website {
-    index_document = "index.html"
+    index_document     = "index.html"
+    error_404_document = "error.html"
   }
 }
 
 #Add index.html to blob storage
-resource "azurerm_storage_blob" "example" {
+resource "azurerm_storage_blob" "indexfile" {
   name                   = "index.html"
   storage_account_name   = azurerm_storage_account.storage_account.name
   storage_container_name = "$web"
   type                   = "Block"
   content_type           = "text/html"
   source                 = "index.html"
+}
+
+resource "azurerm_storage_blob" "errorfile" {
+  name                   = "error.html"
+  storage_account_name   = azurerm_storage_account.storage_account.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  content_type           = "text/html"
+  source                 = "error.html"
 }
